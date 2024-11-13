a3f0b963302bbf9c4c8cd9a9d4e91dc9e6ab0078
ending23 <22890315@ittizimin.edu.mx>
2024-11-12 22:44:10 -0600

    index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tour Por Yucatán</title>
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="body.css">
 </head>
 <body>
 
